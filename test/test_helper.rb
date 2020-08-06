5504e9ad71e7d1cdbdc535710b36436be0baed29
Marcos Essindi <marcessindi@icloud.com>
2020-08-06 12:44:11 +0200

    Coveralls in CI
    Latest version of SimpleCov (with branch coverage) is not compatible with Coveralls.
    Two Gemfiles added: one for development, and one for CI environments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,8 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 if RUBY_VERSION >= "2.7"
   require "simplecov"
   SimpleCov.start do
-    enable_coverage :branch
-    minimum_coverage line: 100, branch: 100
+    enable_coverage :branch unless ENV["CI"]
+    minimum_coverage line: 100, branch: 100 unless ENV["CI"]
   end
 end
 
